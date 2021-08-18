50de5856dd4df5c7e31fa33d4b6d4917dff6f516
RaspberryPi <abhi.anu003@gmail.com>
2021-08-18 12:17:29 +0530

    CORS enabling

--- api.py
+++ api.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask import jsonify
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
 import os.path
 from os import path
 import pandas as pd
 
 app = Flask(__name__)
-api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+#api = Api(app)
 
 @app.route('/hello/', methods=['GET'])
+@cross_origin()
 def welcome():
     return "Hello World!"
 
 @app.route('/rankers/', methods=['GET'])
+@cross_origin()
 def rankers():
     if path.exists("/home/pi/Desktop/FlaskIT-M_API/rankers.csv"):
         data = pd.read_csv('/home/pi/Desktop/FlaskIT-M_API/rankers.csv')
